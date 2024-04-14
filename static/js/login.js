f95758b9c32d1d4050cf8b428ae67defea26d818
SanghyunPark <shpark@mv-w.com>
2024-04-14 15:34:53 +0900

    🐛 FIX: 로그인 페이지 에러 해결

--- static/js/login.js
+++ static/js/login.js
@@ -3,6 +3,17 @@ const usernameInput = document.getElementById('username');
 const passwordInput = document.getElementById('password');
 const signupLink = document.getElementById('signup-link');
 
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('문서가 로드되었습니다.');
+  var signupButton = document.getElementById('signup-link');
+  console.log(signupButton); // 버튼 요소가 올바르게 선택되었는지 확인
+  signupButton.addEventListener('click', function () {
+      console.log('회원가입 버튼이 클릭되었습니다.'); // 클릭 이벤트가 발생하는지 확인
+      window.location.href = '../../accounts/register.html';
+  });
+});
+
+
 loginForm.addEventListener('submit', async (event) => {
   event.preventDefault();
 
@@ -28,7 +39,7 @@ loginForm.addEventListener('submit', async (event) => {
       localStorage.setItem('userToken', data.token);
 
       // 예시: 사용자를 홈페이지로 리디렉션
-      window.location.href = '/main';
+      window.location.href = '../posts/post_list.html';
     } else {
       const error = await response.json();
       console.error('로그인 실패:', error);
