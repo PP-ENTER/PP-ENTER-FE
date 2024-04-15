3af9254ad45e218d7b0bcd9c8ce59f28c17db5e2
Eileen <leh7875@naver.com>
2024-04-15 15:13:09 +0900

    push 작업전

--- static/js/post_create.js
+++ static/js/post_create.js
@@ -1,22 +1,22 @@
-window.onload = function() {
-  document.getElementById('postForm').addEventListener('submit', function(e) {
-    e.preventDefault();
+window.addEventListener('load', function() {
+  const postForm = document.getElementById('postForm');
 
+  postForm.addEventListener('submit', function(e) {
+    e.preventDefault();
     const title = document.getElementById('title').value;
     const content = document.getElementById('content').value;
     const photo = document.getElementById('photo').files[0];
     const tags = document.getElementById('tag').value.split(',').map(tag => tag.trim());
-
     const formData = new FormData();
     formData.append('title', title);
     formData.append('content', content);
     formData.append('photo', photo);
-    tags.forEach(tag => formData.append('tags', tag));
+    tags.forEach(tag => formData.append('tag', tag));
 
     // 로컬 스토리지에서 access 토큰 가져오기
     const accessToken = localStorage.getItem('accessToken');
 
-    fetch('http://43.200.108.45/posts/post_create/', {
+    fetch('http://43.200.108.45:8000/posts/create/', { 
       method: 'POST',
       headers: {
         'Authorization': 'Bearer ' + accessToken
@@ -24,41 +24,23 @@ window.onload = function() {
       body: formData
     })
     .then(response => {
-      if (response.status === 401) {
-        // access 토큰이 만료되었을 경우 refresh 토큰을 사용하여 새로운 access 토큰 발급받기
-        const refreshToken = localStorage.getItem('refreshToken');
-        return fetch('http://43.200.108.45/token/refresh/', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            refresh: refreshToken
-          })
-        })
-        .then(response => response.json())
-        .then(data => {
-          // 새로 발급받은 access 토큰 저장
-          localStorage.setItem('accessToken', data.access);
-          // 새로운 access 토큰으로 다시 요청 보내기
-          return fetch('http://43.200.108.45/posts/post_create/', {
-            method: 'POST',
-            headers: {
-              'Authorization': 'Bearer ' + data.access
-            },
-            body: formData
-          });
+      // 서버에서 JSON 응답을 반환하는 경우
+      if (response.ok) {
+        return response.json();
+      } else {
+        // 서버에서 HTML 문서 응답을 반환하는 경우
+        return response.text().then(text => {
+          throw new Error(`HTTP error! status: ${response.status}\n${text}`);
         });
       }
-      return response;
     })
-    .then(response => response.json())
     .then(data => {
+      // JSON 데이터 처리
       const postId = data.id;
       window.location.href = `/post_detail.html?postId=${postId}`;
     })
-    .catch((error) => {
+    .catch(error => {
       console.error('Error:', error);
     });
   });
-};
+});
